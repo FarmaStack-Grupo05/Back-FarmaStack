e5406bffb22940fe41d5387ecdfa843cae3922e4
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-04-24 22:05:47 -0500

    arrange bug in the getProduct

--- src/handlers/products/getHandler.js
+++ src/handlers/products/getHandler.js
@@ -1,9 +1,9 @@
 const { getProducts } = require("../../controllers/productsControl");
 
 const getHandler = async (req, res) => {
-	let results = await getProducts(req.body);
 	try {
-		res.status(200).json(req.body);
+		const result = await getProducts();
+		res.status(200).json(result);
 	} catch (error) {
 		res.status(400).send(error.message);
 	}
--- src/models/Products.js
+++ src/models/Products.js
@@ -23,7 +23,7 @@ module.exports = (sequelize) => {
 				allowNull: false,
 			},
 			image: {
-				type: DataTypes.STRING,
+				type: DataTypes.TEXT,
 				allowNull: true,
 				defaultValue:
 					"https://previews.123rf.com/images/bsd555/bsd5551709/bsd555170901502/87239039-mano-abierta-con-icono-lineal-de-drogas-ilustraci%C3%B3n-de-l%C3%ADnea-delgada-suministro-de-medicamentos.jpg",
