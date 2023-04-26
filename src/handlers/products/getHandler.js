fa370269bb4c6ec3f39dd22d929903f9c24e28f2
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-04-25 20:50:42 -0500

    para crear la rama David

--- src/handlers/products/getHandler.js
+++ src/handlers/products/getHandler.js
@@ -1,8 +1,17 @@
 const { getProducts } = require("../../controllers/productsControl");
+const { Products } = require("../../db");
+const seeders = require("./seeders/seeders.json");
+
+const fillDataBase = () => {
+	seeders.products.forEach((product) => {
+		Products.create(product);
+	});
+};
 
 const getHandler = async (req, res) => {
 	try {
 		const result = await getProducts(req);
+		result.length === 0 && fillDataBase();
 		res.status(200).json(result);
 	} catch (error) {
 		res.status(400).send(error.message);
