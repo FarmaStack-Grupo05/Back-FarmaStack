1c7dc0fc09436b56a018e2b1931a74b8db71bde3
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-05-09 14:31:43 -0500

    for update branch

--- src/handlers/cart/postCartItem.js
+++ src/handlers/cart/postCartItem.js
@@ -1,15 +1,22 @@
-const { addCartItem } = require("../../controllers/cartController/cartController")
+const {
+	addCartItem,
+} = require("../../controllers/cartController/cartController");
 
 const addProductHandler = async (req, res) => {
-  const { productId, userId, quantity } = req.body
+	const { productId, userId, quantity } = req.body;
+	console.log(productId, userId);
 
-  try {
-    const result = await addCartItem(userId, productId, quantity ? Number.parseInt(quantity) : 1)
-    res.status(200).json(result)
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error.message)
-  }
-}
+	try {
+		const result = await addCartItem(
+			userId,
+			productId,
+			quantity ? Number.parseInt(quantity) : 1
+		);
+		res.status(200).json(result);
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error.message);
+	}
+};
 
-module.exports = addProductHandler
+module.exports = addProductHandler;
