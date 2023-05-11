635c543240cbef5b3a24ad0e9cba7a931919a61c
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-05-10 23:41:20 -0500

    se agrego stock al modelo y a los seeders tambien se agrego la ruta de updateProduct tambien en el get de user

--- src/controllers/userController/getController.js
+++ src/controllers/userController/getController.js
@@ -2,16 +2,19 @@ const { User } = require("../../db");
 const { Op } = require("sequelize");
 
 const getController = async (req) => {
-  if (req.query === "name") {
-    const result = await User.findAll({
-      where: {
-        name: { [Op.iLike]: `%${req.query}%` },
-      },
-    });
-    return result;
-  }
-  const result = await User.findAll();
-  return result;
+	const { email } = req.query;
+	if (email) {
+		let result = await User.findAll({
+			where: {
+				email,
+			},
+		});
+
+		return result;
+	} else {
+		let result = await User.findAll();
+		return result;
+	}
 };
 
 module.exports = getController;
