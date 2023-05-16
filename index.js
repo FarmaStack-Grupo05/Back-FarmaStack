47058c3d8a65e68d154392caac556efbb89ea7d2
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-05-15 19:18:42 -0500

    for update branch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 const { conn } = require("./src/db.js");
-const { Products } = require("./src/db");
+const { Products, User } = require("./src/db");
 const seeders = require("./seeders/seeders.json");
 
 const fillDataBase = () => {
@@ -10,9 +10,25 @@ const fillDataBase = () => {
 	});
 };
 
+const setUserAdmin = () => {
+	User.findOrCreate({
+		where: {
+			email: "farmastackdrogueria@gmail.com",
+		},
+		defaults: {
+			name: "Farmastack",
+			address: "cll falsa 123",
+			phone: "3224566789",
+			rol: "admin",
+			active: true,
+		},
+	});
+};
+
 conn.sync({ force: true }).then(() => {
 	app.listen(3001, () => {
 		fillDataBase();
+		setUserAdmin();
 		console.log("listening at 3001");
 	});
 });
